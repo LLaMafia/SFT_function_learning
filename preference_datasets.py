b00cf5940d248d041e3fcc08a6fb41f8f0e5a6b5
Shangmin Guo <shawnguo.cn@gmail.com>
2024-01-02 17:04:36 +0000

    added sft data generation

--- preference_datasets.py
+++ preference_datasets.py
@@ -19,104 +19,6 @@ def extract_anthropic_prompt(prompt_and_response):
     return prompt_and_response[:search_term_idx + len(search_term)]
 
 
-def strip_html_tags(html_string):
-    """Strip HTML tags from a string, except for <code> tags (which contain real code in the StackExchange answers)."""
-    # Create a BeautifulSoup object
-    soup = BeautifulSoup(html_string, 'html.parser')
-
-    # Initialize an empty list to store the text
-    text = []
-    for element in soup.children:
-        if isinstance(element, NavigableString):
-            continue
-        if element.name == 'p':
-            text.append(''.join(child.string for child in element.children if isinstance(child, NavigableString)))
-        elif element.name == 'pre':
-            for code in element.find_all('code'):
-                text.append("<code>" + code.get_text() + "</code>")
-        elif element.name == 'code':
-            text.append("<code>" + element.get_text() + "</code>")
-
-    # Join the text together with newlines in between
-    text = "\n\n".join(text)
-
-    return text
-
-
-def get_se(split, silent=False, cache_dir: str = None) -> Dict[str, Dict[str, Union[List[Tuple[int, int]], List[str], str]]]:
-    """Load the StackExchange dataset from Huggingface, and return a dict of prompts and responses. See get_hh for the format.
-    
-       We strip the HTML tags from the responses (except for <code> tags), and we add necessary newlines.
-    """
-    print(f'Loading SE dataset ({split} split) from Huggingface...')
-    dataset = datasets.load_dataset('HuggingFaceH4/stack-exchange-preferences', cache_dir=cache_dir)['train']
-    print('done')
-
-    # shuffle the dataset and select 1% for test
-    dataset = dataset.shuffle(seed=42)
-    dataset = dataset.select(range(int(len(dataset) * 0.01))) if split == 'test' else dataset.select(
-        range(int(len(dataset) * 0.01), len(dataset)))
-
-    def strip_html(x):
-        x['question'] = strip_html_tags(x['question'])
-        for a in x['answers']:
-            a['text'] = strip_html_tags(a['text'])
-        return x
-
-    dataset = dataset.map(strip_html, num_proc=64)
-
-    data = defaultdict(dict)
-    for row in tqdm.tqdm(dataset, desc='Processing SE', disable=silent):
-        prompt = '\n\nHuman: ' + row['question'] + '\n\nAssistant:'
-        responses = [' ' + a['text'] for a in row['answers']]
-        scores = [a['pm_score'] for a in row['answers']]
-
-        pairs = []
-        for i in range(len(responses)):
-            for j in range(i + 1, len(responses)):
-                pairs.append((i, j) if scores[i] > scores[j] else (j, i))
-
-        data[prompt]['responses'] = responses
-        data[prompt]['pairs'] = pairs
-        data[prompt]['sft_target'] = max(responses, key=lambda x: scores[responses.index(x)])
-
-    return data
-
-def get_shp(split: str, silent: bool = False, cache_dir: str = None) -> Dict[str, Dict[str, Union[List[Tuple[int, int]], List[str], str]]]:
-    """Load the Stanford Human Preferences dataset from Huggingface and convert it to the necessary format. See hh for the format.
-
-       We filter preference pairs to only keep pairs where the score ratio is at least 2.
-       For this dataset, the sft_target is the response with the highest score.
-    """
-    print(f'Loading SHP dataset ({split} split) from Huggingface...')
-    dataset = datasets.load_dataset('stanfordnlp/SHP', split=split, cache_dir=cache_dir)
-    print('done')
-
-    data = defaultdict(lambda: defaultdict(list))
-    for row in tqdm.tqdm(dataset, desc='Processing SHP', disable=silent):
-        prompt = '\n\nHuman: ' + row['history'] + '\n\nAssistant:'
-        responses = [' ' + row['human_ref_A'], ' ' + row['human_ref_B']]
-        scores = [row['score_A'], row['score_B']]
-        if prompt in data:
-            n_responses = len(data[prompt]['responses'])
-        else:
-            n_responses = 0
-        score_ratio = max(scores[0] / scores[1], scores[1] / scores[0])
-        if score_ratio < 2:
-            continue
-
-        # according to https://huggingface.co/datasets/stanfordnlp/SHP
-        data[prompt]['pairs'].append((n_responses, n_responses + 1) if row['labels'] == 1 else (n_responses + 1, n_responses))
-        data[prompt]['responses'].extend(responses)
-        data[prompt]['scores'].extend(scores)
-
-    for prompt in data:
-        data[prompt]['sft_target'] = max(data[prompt]['responses'], key=lambda x: data[prompt]['scores'][data[prompt]['responses'].index(x)])
-        del data[prompt]['scores']
-
-    return data
-
-
 def get_hh(split: str, silent: bool = False, cache_dir: str = None) -> Dict[str, Dict[str, Union[List[Tuple[int, int]], List[str], str]]]:
     """Load the Anthropic Helpful-Harmless dataset from Huggingface and convert it to the necessary format.
     
@@ -160,18 +62,20 @@ def get_hh(split: str, silent: bool = False, cache_dir: str = None) -> Dict[str,
     return data
 
 
-def get_dataset(name: str, split: str, silent: bool = False, cache_dir: str = None):
+def get_dataset(
+        name: str,
+        split: str,
+        silent: bool = False,
+        cache_dir: str = './data/helpful-base/'
+):
     """Load the given dataset by name. Supported by default are 'shp', 'hh', and 'se'."""
-    if name == 'shp':
-        data = get_shp(split, silent=silent, cache_dir=cache_dir)
-    elif name == 'hh':
+    if name == 'hh':
         data = get_hh(split, silent=silent, cache_dir=cache_dir)
-    elif name == 'se':
-        data = get_se(split, silent=silent, cache_dir=cache_dir)
     else:
         raise ValueError(f"Unknown dataset '{name}'")
 
-    assert set(list(data.values())[0].keys()) == {'responses', 'pairs', 'sft_target'}, \
+    assert set(list(data.values())[0].keys()) \
+           == {'responses', 'pairs', 'sft_target'}, \
         f"Unexpected keys in dataset: {list(list(data.values())[0].keys())}"
 
     return data
